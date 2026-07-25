e3fcdf7eacc4528d9fc1b4eee384107752671043
Kristy Mae Almuete <kristyalmuete@gmail.com>
2026-07-26 02:15:07 +0800

    feat(landing): setup initial recipe index

--- apps/cookbook/src/app/app.ts
+++ apps/cookbook/src/app/app.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
+import {
+  NbBadge,
+  NbButton, NbCallout,
+  NbCard,
+  NbCardActions,
+  NbCardContent,
+  NbCardDescription,
+  NbCardHeader,
+  NbCardTitle, NbCluster, NbDisplay, NbHalftone, NbSeparator, NbStack, NbSticker, NbText
+} from '@ng-brutalism/ui';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { tablerBrandGithub } from '@ng-icons/tabler-icons';
 
 @Component({
-  imports: [RouterModule],
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  templateUrl: 'app.html',
+  imports: [
+    NbCard,
+    NbCardHeader,
+    NbCardTitle,
+    NbCardDescription,
+    NbCardContent,
+    NbCardActions,
+    NbButton,
+    NbStack,
+    NbBadge,
+    NbDisplay,
+    NbCallout,
+    NbCluster,
+    NgOptimizedImage,
+    NbText,
+    NbSeparator,
+    NgIcon,
+    NbHalftone,
+    NbSticker
+  ],
+  providers: [
+    provideIcons({ tablerBrandGithub })
+  ],
+  host: {
+    class: 'mx-auto flex justify-center w-full'
+  }
 })
 export class App {
   protected title = 'cookbook';
